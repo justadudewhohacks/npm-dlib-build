6595ab5d4a089b4073504f100d29cba6d711020f
vincent <muehler.v@gmail.com>
2018-01-07 19:13:22 +0100

    fix: export correct include dir

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { dlibRoot, dlibLocalLib } = require('./constants')
+const { dlibSrc, dlibLocalLib } = require('./constants')
 
 module.exports = {
-  dlibIncludeDir: dlibRoot,
+  dlibIncludeDir: dlibSrc,
   dlibLib: dlibLocalLib
 }

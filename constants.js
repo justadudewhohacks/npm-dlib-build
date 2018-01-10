d6a1ca267d13a67e1539be2c8c655cc2d7d586f7
vincent <muehler.v@gmail.com>
2018-01-10 22:12:23 +0100

    export static library path

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ const rootDir = __dirname
 const dlibRoot = path.join(rootDir, 'dlib')
 const dlibSrc = path.join(dlibRoot, 'dlib')
 const dlibBuild = path.join(dlibRoot, 'build')
-const dlibLocalLib = isWin() ? path.join(dlibBuild, 'dlib/Release/dlib.lib') : path.join(dlibBuild, 'dlib/libdlib.so.19.8.0')
+const dlibLocalLib = isWin() ? path.join(dlibBuild, 'dlib/Release/dlib.lib') : path.join(dlibBuild, 'dlib/libdlib.a')
 
 module.exports = {
   rootDir,

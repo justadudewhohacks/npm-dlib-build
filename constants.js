2b54ae5bf53659363d26688e9c39ef0c023e2f9b
vincent <muehler.v@gmail.com>
2018-01-11 12:59:40 +0100

    fix dlibLibDir

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ const rootDir = __dirname
 const dlibRoot = path.join(rootDir, 'dlib')
 const dlibSrc = path.join(dlibRoot, 'dlib')
 const dlibBuild = path.join(dlibRoot, 'build')
-const dlibLibDir = isWin() ? path.join(dlibBuild, 'dlib/Release') : dlibBuild
+const dlibLibDir = isWin() ? path.join(dlibBuild, 'dlib/Release') : path.join(dlibBuild, 'dlib')
 const dlibLocalLib = isWin() ? path.join(dlibLibDir, 'dlib.lib') : path.join(dlibLibDir, 'libdlib.a')
 
 module.exports = {
